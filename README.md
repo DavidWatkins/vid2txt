3ab4c6ae1ae653b6b214457844d29e0a0a454114
David Watkins <djw2146@columbia.edu>
2017-02-13 13:32:35 -0500

    Added html output and mp4 to txt functionality

--- README.md
+++ README.md
@@ -6,17 +6,32 @@ Mp4 file to ascii text files
 DEMO
 ----
 
-![](http://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_5mb.mp4)
+Gangnam Style
+
+HTML: http://hit9.github.io/gif2txt/gangnam-style.html
 
 USAGE
 -----
 
 ```
-./gif2txt.py test.gif -m 80 -o images
+usage: gif2txt.py [-h] [-m MAXLEN] [-o OUTPUT] [-html HTML] filename
+
+positional arguments:
+  filename              Mp4 input file
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -m MAXLEN, --maxLen MAXLEN
+                        Max width of the output txt video
+  -o OUTPUT, --output OUTPUT
+                        Name of the output directory
+  -html HTML, --html HTML
+                        Name of the html output file
+
 ```
-where images is a directory
 
 Requirements
 -----------
 
 * PIL
+* cv2
--- gif2txt.py
+++ gif2txt.py
@@ -1,73 +1,99 @@
-#! /usr/bin/env python3
+#! /usr/bin/env python
 import argparse
 from PIL import Image
+import cv2
 from jinja2 import Template
 
+def extract_frames(video_file_name):
+    vidcap = cv2.VideoCapture(video_file_name)
+    success, image = vidcap.read()
+    count = 0
+    success = True
+    frames = []
+    fps = vidcap.get(cv2.cv.CV_CAP_PROP_FPS)
+    frame_interval = 1.0/fps * 1000
 
-def gif2txt(filename, maxLen=80, output_file='out.html'):
+    while success:
+        success, image = vidcap.read()
+        frames.append(image)
+        count += 1
+
+    print("Writing %d frames @ %d fps" % (count, fps))
+    return frames, frame_interval
+
+def write_frames_to_files(video_file_name, frames, out_dir):
+    count = 0
+    for frame in frames:
+        #print("Writing file " + out_dir + video_file_name + '.frame%d.jpg' % count)
+        cv2.imwrite(out_dir + video_file_name + '.frame%d.jpg' % count, frame)
+        count += 1
+
+def frames2txt(frames, maxLen=80):
     try:
         maxLen = int(maxLen)
     except:
         maxLen = 80
 
-    chs = "MNHQ$OC?7>!:-;. "
+    chs = "#MNHQ$OC?7>!:-;. "
+    #chs = "  ..;-:!>7?irl+heaqpdb0wgm@"[::-1]
 
-    try:
-        img = Image.open(filename)
-    except IOError:
-        exit("file not found: {}".format(filename))
+    txt_images = []
+    for frame in frames:
+        if frame is None:
+            continue
+        height, width, x = frame.shape
+        rate = float(maxLen) / max(width, height)
+        width = int(rate * width)
+        height = int(rate * height)
 
-    width, height = img.size
-    rate = float(maxLen) / max(width, height)
-    width = int(rate * width)
-    height = int(rate * height)
+        im = Image.fromarray(frame)
+        im = im.resize((width, height))
+        string = ''
+        for h in range(height):
+            for w in range(width):
+                rgb = im.getpixel((w, h))
+                string += chs[int(sum(rgb) / 3.0 / 256.0 * len(chs))]
+            string += '\n'
+        txt_images.append(string)
+    
+    return txt_images
 
-    palette = img.getpalette()
-    strings = []
+def write_txt_frames_to_files(video_file_name, txt_frames, out_dir):
+    count = 0
+    for frame in txt_frames:
+        #print("Writing file " + out_dir + video_file_name + '.frame%d.txt' % count)
+        with open(out_dir + video_file_name + '.frame%d.txt' % count, 'w') as txt_file:
+            txt_file.write(frame)
+        count += 1
+    print("Wrote %d txt images to directory %s" % (len(txt_frames), out_dir))
 
-    try:
-        while 1:
-            img.putpalette(palette)
-            im = Image.new('RGB', img.size)
-            im.paste(img)
-            im = im.resize((width, height))
-            string = ''
-            for h in range(height):
-                for w in range(width):
-                    rgb = im.getpixel((w, h))
-                    string += chs[int(sum(rgb) / 3.0 / 256.0 * 16)]
-                string += '\n'
-            strings.append(string)
-            img.seek(img.tell() + 1)
-    except EOFError:
-        pass
-
+def write_txt_frames_to_html(out_filename, txt_frames, frame_interval):
     with open('template.jinja') as tpl_f:
         template = Template(tpl_f.read())
-        html = template.render(strings=strings)
-    with open(output_file, 'w') as out_f:
+        html = template.render(strings=txt_frames, frame_interval=frame_interval)
+    with open(out_filename, 'w') as out_f:
         out_f.write(html)
-
+    print("Finished writing html file %s" % out_filename)
 
 def main():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('filename',
-                        help='Gif input file')
-    parser.add_argument('-m', '--maxLen', type=int,
-                        help='Max width of the output gif')
-    parser.add_argument('-o', '--output',
-                        help='Name of the output file')
+    parser.add_argument('filename', help='Mp4 input file')
+    parser.add_argument('-m', '--maxLen', type=int, help='Max width of the output txt video')
+    parser.add_argument('-o', '--output', help='Name of the output directory')
+    parser.add_argument('-html', '--html', help='Name of the html output file')
     args = parser.parse_args()
 
     if not args.maxLen:
         args.maxLen = 80
     if not args.output:
-        args.output = 'out.html'
-
-    gif2txt(filename=args.filename,
-            maxLen=args.maxLen,
-            output_file=args.output)
+        args.output = 'images/'
+    
+    frames, frame_interval = extract_frames(args.filename)
+    txt_frames = frames2txt(frames, args.maxLen)
+    write_txt_frames_to_files(args.filename, txt_frames, args.output)
+    if args.html:
+        write_txt_frames_to_html(args.html, txt_frames, frame_interval)
 
 if __name__ == '__main__':
     main()
